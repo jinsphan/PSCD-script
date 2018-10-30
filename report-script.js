878c497289df7418f08c1e3afb08dd322f340ae2
jinsphan <pvtinh1996@gmail.com>
2018-10-30 22:19:12 +0700

    add promise

--- report-script.js
+++ report-script.js
@@ -7,36 +7,44 @@ template: [1, "08:00", 8, "17:00", "your job is here", "22/10/2017"],
 
 // TODAY
 let data = [
-
 ];
 
 // ALL
 
-
-data.map(rp => {
-    $.ajax({
-        url: "http://pscd.pacificsoftdev.com/reports/add",
-        method: "POST",
-        data: {
-            "report[project_id]": rp[0],
-            "report[time_start]": rp[1],
-            "report[work_time]": rp[2],
-            "report[time_end]": rp[3],
-            "report[job]": rp[4],
-            "report[notes]": rp[5],
-            "btn_submit": "Add"
-        },
-        success: data => {
-            console.log(data);
-        },
-        error: er => {
-            console.log(er)
-        }
+Promise.all(data.map(rp => {
+    return new Promise((rs, rj) => {
+        $.ajax({
+            url: "http://pscd.pacificsoftdev.com/reports/add",
+            method: "POST",
+            data: {
+                "report[project_id]": rp[0],
+                "report[time_start]": rp[1],
+                "report[work_time]": rp[2],
+                "report[time_end]": rp[3],
+                "report[job]": rp[4],
+                "report[notes]": rp[5],
+                "btn_submit": "Add"
+            },
+            success: data => {
+                rs(rp);
+            },
+            error: er => {
+                rj(rp);
+            }
+        })
     })
-})
+}))
+    .then(res => {
+        console.log("DONE", res);
+    })
+    .catch(rpEr => {
+        console.log("REPORT HAS ERROR", rpEr);
+    })
+
+
 
 // Backup data
 let bk = [
-[41, "08:00", 6, "15:00", "dev game play page", "30/10/2017"],	
-[36, "15:00", 2, "17:00", "dev game play page", "30/10/2017"],
+    [41, "08:00", 6, "15:00", "dev game play page", "30/10/2017"],
+    [36, "15:00", 2, "17:00", "dev game play page", "30/10/2017"],
 ];
